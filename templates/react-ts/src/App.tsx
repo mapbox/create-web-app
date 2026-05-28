ae2cd7bc27dfa59983f397a579cb626b166d79a8
Andrew Sepic <andrew.sepic@mapbox.com>
2026-05-28 10:09:08 -0400

    Added search enabled typescript template

--- templates/react-ts/src/App.tsx
+++ templates/react-ts/src/App.tsx
@@ -1,33 +1,29 @@
 import { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 
-import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
 import './App.css'
 
 function App() {
-
-  const mapRef = useRef<mapboxgl.Map>(null)
+  const mapRef = useRef<mapboxgl.Map | null>(null)
   const mapContainerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN as string
+    const token = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
 
     mapRef.current = new mapboxgl.Map({
+      accessToken: token,
       container: mapContainerRef.current!,
-      center:  [-71.05953, 42.36290],
+      center: [-71.05953, 42.36290],
       zoom: 13,
-    });
+    })
 
     return () => {
       mapRef.current?.remove()
     }
   }, [])
 
-
-return (
-    <div id='map-container' ref={mapContainerRef} />
-  )
+  return <div id='map-container' ref={mapContainerRef} />
 }
 
 export default App
-
